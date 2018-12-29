70f2fb42ab0d8bdf2965c78273be0c95749f5e21
Artemiy Surov <artemsurov@gmail.com>
2018-12-30 01:05:11 +0300

    only methods without view

--- hw6/src/polls/urls.py
+++ hw6/src/polls/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import index
+from .views import index, detail, vote, results
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("<int:qid>/", detail, name="detail"),
+    path("<int:qid>/vote/", vote, name="vote"),
+    path("<int:qid>/results/", results, name="results")
+
 ]
--- hw6/src/polls/views.py
+++ hw6/src/polls/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
+from .models import Question, Choice
 
 
 # Create your views here.
 def index(requests):
-    return HttpResponse("BlaBlaBla Text Here")
+    question_list = Question.objects.order_by('-pub_data')[:5]
+    response = ", "
+    for question in question_list:
+        response = question.question_text + response
+    return render(requests, 'polls/index.html', {'latest_question_list': question_list})
+
+
+def detail(request, qid):
+    question = get_object_or_404(Question, pk=qid)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def vote(request, qid):
+    question = get_object_or_404(Question, pk=qid)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.htlm', {'question': question, 'error_message': 'Make your choice bitch'})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
+
+
+def results(request, qid):
+    question = get_object_or_404(Question, pk=qid)
+    return render(request, 'polls/results.html', {'question': question})
